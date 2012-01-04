9ab5bac73ac7764051b0a62cd9f072880fa34311
Jacco Flenter <jacco@secretcodemachine.com>
2012-01-04 09:57:21 +0100

    setup.py now installs django-scm_core.

--- setup.py
+++ setup.py
@@ -10,10 +10,14 @@ setup(
         name='facebook-tab-pages',
         version=".".join(map(str, __import__('fb_tabs').__version__)),
         author='Jacco flenter @ secretcodemachine',
+        dependency_links=[
+            'http://bitbucket.org/flenter/django-scm_core/get/default.tar.gz#egg=django-scm_core'
+        ],
         description='Link a view to an app/tabb on facebook',
         url='http://github.com/flenter/facebook-tab-pages',
         install_requires=[
-            'django',
+            'django>=1.3',
+            'django-scm_core>=0.1.1',
             ],
         packages=find_packages(),
         include_package_data=True,
